660ba1354d975f99025cc389ed8555de0a294058
Jader Dias <jaderd@gmail.com>
2015-01-05 00:53:51 +0100

    added zip support

--- generate_db.pl
+++ generate_db.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use IO::Uncompress::Unzip qw(unzip $UnzipError);
+use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
 
 my $num_args = scalar @ARGV;
 if($num_args != 1) {
@@ -11,12 +12,16 @@ if($num_args != 1) {
 
 my $ngram_file_name = $ARGV[0];
 
-my $z = new IO::Uncompress::Unzip $ngram_file_name
-    or die "unzip failed: $UnzipError\n";
+my $class = "IO::Uncompress::Gunzip";
+$class = "IO::Uncompress::Unzip" if($ngram_file_name =~ /\.zip$/);
+my $uncompress = new $class $ngram_file_name
+        or die "can't open $ngram_file_name";
 
 my $previous_word;
 my $previous_count = 0;
-while(my $line = $z->getline()) {
+my %ngrams = ();
+while(my $line = $uncompress->getline()) {
+    print "$line";
     next unless($line  =~ /^([A-Za-z][a-z]+)\s+\d+\s+(\d+)\s/);
     my ($current_word, $current_count) = ($1, $2);
     if(($previous_word // '') eq $current_word) {
@@ -24,10 +29,15 @@ while(my $line = $z->getline()) {
         next;
     }
 
-    print "$previous_word\t$previous_count\n";
+    $ngrams{$previous_word} = $previous_count;
     $previous_word = $current_word;
     $previous_count = 0;
 }
 
-$z->close();
+$uncompress->close();
 
+my @words = sort { $ngrams{$b} <=> $ngrams{$a} } keys %ngrams;
+
+for my $word (@words) {
+    print "$word\n";
+}
