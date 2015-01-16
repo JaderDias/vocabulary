310407f641ec4a4513c779d9eb95c5aa213d4190
Jader Dias <jaderd@gmail.com>
2015-01-16 22:11:13 +0100

    added support to multiple input files, limited output to 40000 words

--- generate.pl
+++ generate.pl
@@ -5,38 +5,45 @@ use IO::Uncompress::Unzip qw(unzip $UnzipError);
 use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
 use JSON qw(to_json);
 
-my $ngram_file_name = $ARGV[0];
-my $class;
-if($ngram_file_name =~ /\.zip$/) {
-        $class = "IO::Uncompress::Unzip";
-} else {
-        $class = "IO::Uncompress::Gunzip";
-}
-
-my $uncompress = new $class $ngram_file_name
-        or die "can't open $ngram_file_name";
-
+my $max_words = 40000;
 my $count = 0;
 my $previous_word;
 my $previous_count = 0;
 my %ngrams = ();
-while(my $line = $uncompress->getline()) {
-    $count++;
-    print "from $count lines we got " . (scalar keys %ngrams) . " words\n" if ($count % 1e5) == 0;
-    next unless($line  =~ /^([a-z]+)\s+(\d+)\s+(\d+)\s/);
-    my ($current_word, $year, $current_count) = ($1, $2, $3);
-    next if $year < 1970;
-    if(($previous_word // '') eq $current_word) {
-        $previous_count += $current_count;
-        next;
-    }
 
-    $ngrams{$previous_word} = $previous_count if $previous_word;
-    $previous_word = $current_word;
-    $previous_count = 0;
+while(my $ngram_file_name = shift @ARGV) {
+        my $class;
+        if($ngram_file_name =~ /\.zip$/) {
+                $class = "IO::Uncompress::Unzip";
+        } else {
+                $class = "IO::Uncompress::Gunzip";
+        }
+
+        my $uncompress = new $class $ngram_file_name
+                or die "can't open $ngram_file_name";
+
+        while(my $line = $uncompress->getline()) {
+            $count++;
+            print "from $count lines we got " . (scalar keys %ngrams) . " words\n" if ($count % 1e7) == 0;
+            next unless($line  =~ /^([a-z]+)\s+(\d+)\s+(\d+)\s/);
+            my ($current_word, $year, $current_count) = ($1, $2, $3);
+            next if $year < 1970;
+            if(($previous_word // '') eq $current_word) {
+                $previous_count += $current_count;
+                next;
+            }
+
+            $ngrams{$previous_word} = $previous_count if $previous_word;
+            $previous_word = $current_word;
+            $previous_count = 0;
+        }
+
+        $uncompress->close();
+        print "after finishing reading $ngram_file_name with $count lines we found " . (scalar keys %ngrams) . " words\n";
+        my @words = sort { $ngrams{$b} <=> $ngrams{$a} } keys %ngrams;
+        %ngrams = map { $_ => $ngrams{$_} } @words[0..$max_words];
+        print "reduced to " . (scalar keys %ngrams) . " words\n";
 }
-
-$uncompress->close();
 
 my @words = sort { $ngrams{$b} <=> $ngrams{$a} } keys %ngrams;
 my $output = 'public/dictionary.json';
